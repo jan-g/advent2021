33c33fb4e1d2a5773d1641db5327fd8e8cd06a69
jang <jang@ioctl.org>
2021-12-21 09:41:33 +0000

    Day 21

--- test21-25/Spec.hs
+++ test21-25/Spec.hs
@@ -27,4 +27,10 @@ main =
   hspec $ do
     describe "Day 21" $ do
       it "correctly runs on the test data" $ do
-        True `shouldBe` True
+        Day21.move 0 Day21.die [4, 8] [0,0] `shouldBe` (993, 745)
+
+      it "correctly works out the scores for the two players" $ do
+        let (w1, w2) = Day21.move' (Map.singleton ((0,4), (0,8)) 1) (0,0)
+        min w1 w2 `shouldBe` 341960390180808
+        max w1 w2 `shouldBe` 444356092776315
+      
